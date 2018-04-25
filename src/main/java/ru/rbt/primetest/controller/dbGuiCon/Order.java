d5ddb0e4176e7b27e4ba4d35acc4c8d58605a3fa
Maxim Britikov <BritikovMI@rbtechnologies.ru>
2018-04-25 16:56:43 +0300

    fstSteps1

--- src/main/java/ru/rbt/primetest/controller/dbGuiCon/Order.java
+++ src/main/java/ru/rbt/primetest/controller/dbGuiCon/Order.java
@@ -3,41 +3,31 @@ package ru.rbt.primetest.controller.dbGuiCon;
 import java.io.Serializable;
 
 public class Order implements Serializable{
-    public static String id_pk;
-    public String date_of;
-    public String customer_Id;
+    private int id;
+    private String date;
+    private int customer;
 
-    public Order() {}
-
-    public static void setId_pk(String id_pk) {
-        Order.id_pk = id_pk;
+    public long getId() {
+        return id;
     }
 
-    public void setDate_of(String date_of) {
-        this.date_of = date_of;
+    public void setId(int id) {
+        this.id = id;
     }
 
-    public void setCustomer_Id(String customer_Id) {
-        this.customer_Id = customer_Id;
+    public String getDate() {
+        return date;
     }
 
-    public Order(String id_pk, String date_of, String customer_Id){
-        this.id_pk = id_pk;
-
-        this.date_of = date_of;
-        this.customer_Id = customer_Id;
-    };
-
-    public static String getId_pk() {
-        return id_pk;
+    public void setDate(String date) {
+        this.date = date;
     }
 
-    public String getDate_of() {
-        return date_of;
+    public int getCustomer() {
+        return customer;
     }
 
-    public String getCustomer_Id() {
-        return customer_Id;
+    public void setCustomer(int customer) {
+        this.customer = customer;
     }
-
 }
--- src/main/java/ru/rbt/primetest/controller/testService/BasicView.java
+++ src/main/java/ru/rbt/primetest/controller/testService/BasicView.java
@@ -6,11 +6,9 @@ import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
 import java.io.Serializable;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.List;
 
 import static ru.rbt.primetest.model.jdbc.Main.getDBInf;
@@ -19,17 +17,17 @@ import static ru.rbt.primetest.model.jdbc.Main.getDBInf;
 @ViewScoped
 public class BasicView implements Serializable {
     Connection con;
+    private String roleDesc;
     private ResultSet rs;
-    List context;
     private List<Car> cars;
 
     @ManagedProperty("#{carService}")
     private CarService service;
 
     @PostConstruct
-    public void init() {
+    public void init() throws SQLException {
         cars = service.createCars(5);
-        getDBInf("SHOW_TABLE");
+        displayTableRecords();
     }
     
     public List<Car> getCars() {
@@ -49,13 +47,11 @@ public class BasicView implements Serializable {
         return s;
     }
     private void displayTableRecords()throws SQLException {
-        String query = "SELECT * FROM USERS";
-        PreparedStatement state = con.prepareStatement(query);
-        state.execute();
+        Statement state = con.createStatement();
+        state.executeQuery("SELECT * FROM BRM_ORDER");
         rs = state.getResultSet();
         while(rs.next()){
-            cars = new Car(rs.getInt(1), rs.getString(2), rs.getInt(3);
-
+            roleDesc = rs.getString(2);
         }
 
     }
